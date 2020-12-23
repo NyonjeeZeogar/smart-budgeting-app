1d5ec923eb546f1d52ab067175a71f8182a8bc42
Nyonjee Zeogar <nyonjeezeogar@gmail.com>
2020-12-23 11:33:21 -0600

    Create Quickstart

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const dotenv = require('dotenv');
+const PlaidItem = require("./models/plaid-item");
+const PlaidAccount = require("./models/plaid-account");
+const User = require("./models/user");
 const app = express();
 
 dotenv.config();
@@ -15,21 +18,65 @@ const client = new plaid.client(
     process.env.PLAID_CLIENT_ID,
     process.env.PLAID_SECRET,
     process.env.PLAID_PUBLIC_KEY,
-    palid.environment.sandbox,
+    plaid.environments.sandbox
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname,"src/plaid-link.html"))
+    res.sendFile(path.join(__dirname,"plaid-link.html"));
 });
+
+app.post("/plaid_token_exchange", async (req, res) => {
+    try {
+        const { publicToken } = req.body;
+
+        const { access_token } = await client
+            .exchangePublicToken(publicToken)
+            .catch(console.error);
+
+        const { accounts, item } = await client
+            .getAccounts(access_token)
+            .catch(console.error);
+
+        const user = await User.findOne().exec();
+  
+        const plaidItem = await new PlaidItem({
+            userId: user._id,
+            availableProducts: item.available_products,
+            billedProducts: item.billed_products,
+            institutionId: item.institution_id,
+            itemId: item.item_id,
+            webhook: item.webhook
+        }).save();
+
+        console.log({ user, plaidItem });
+
+        const savedAccounts = accounts.map(
+            async account =>
+                await new PlaidAccount({
+                    plaidItemId: plaidItem._id,
+                    accountId: account.account_id,
+                    mask: account.mask,
+                    balances: account.balances,
+                    name: account.name,
+                    officialName: account.official_name,
+                    subtype: account.subtype,
+                    type: account.type
+                }).save()
+        );
+  
 
 app.post('/plaid_token_exchange', async (req, res) => {
     const { publicToken } = req.body;
 
-    const { access_token } = await client.exchangePublicToken(publicToken).catch(handleError);
-    const { accounts, item } = await client.getAccounts(access_token).catch(handleError);
+    const { access_token } = await client
+        .exchangePublicToken(publicToken)
+        .catch(console.error);
+    const { accounts, item } = await client
+        .getAccounts(access_token)
+        .catch(console.error);
 
     console.log({
         accounts, 
